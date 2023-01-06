19dea236a499b2ca419c564d3714b55c9f56242c
dweorh <dweorh@gmail.com>
2023-01-06 18:14:20 +0100

    #65 + #66 [dev - php] A Network Game - Collisions
    https://youtu.be/z2yzoaHEpD8

---
+++ S01E08/index.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once './class/ChessEngine.php';
+require_once './class/ChessMove.php';
+
+/*
+http://www.inwap.com/pdp10/ansicode.txt
+\e equals chr(27)
+[0m = Clear all special attributes
+[30m = Write with black
+[37m = Write with white
+[40m = Set background to black (GIGI)
+[46m = Set background to cyan
+[47m = Set background to white
+
+[40G = Move to column 40 of current line
+[A = Move up one line, stop at top of screen, [9A = move up 9
+[K = [0K = Erase from current position to end (inclusive)
+*/
+function print_board($board)
+{
+    $patterns = [
+        ["\e[0;30;47m %s \e[0m", "\e[0;37;40m %s \e[0m"],
+        ["\e[0;37;40m %s \e[0m", "\e[0;30;47m %s \e[0m"]
+    ];
+
+    $output = '';
+    $output .= sprintf("\e[0;46m%s\e[0m\n", "  A  B  C  D  E  F  G  H  ");
+    for($i = 0; $i < 64; $i += 8) {
+        for($y = 0; $y < 3; $y++) {
+            $output .= sprintf("\e[46m%s\e[0m", $y == 1 ? 8 - $i / 8 : ' ');
+            for($x = $i; $x <= $i + 7; $x++) {
+                $pattern = $x / 8 % 2 ? $patterns[0] : $patterns[1];
+                $output .= sprintf($x % 2 ? $pattern[0] : $pattern[1], $y == 1 ? $board[$x] : ' ');
+            }
+            $output .= "\e[46m \e[0m";
+            $output .= "\n";
+        }
+    }
+    $output .= sprintf("\e[0;46m%s\e[0m\n", "  A  B  C  D  E  F  G  H  ");
+    return $output;
+}
+
+function perform_action(ChessEngine $game, $action){
+    if ($action == 'exit') {
+        return 0;
+    }
+
+    $move = $game->move($action);
+    $message = '';
+    if($move) {
+        if ($move->valid_axis) {
+            if ($move->king_in_check) {
+                $message = 'The King in check! ' . $move->figure_from . ' -> ' . $action;
+            } else if ($move->collision == ChessEngine::MOVE_UNKNOWN) {
+                $message = 'Unknow move! ' . $move->figure_from . ' -> ' . $action;
+            } else if ($move->collision == ChessEngine::MOVE_FRIENDLY_FIRE) {
+                $message = 'Friendly fire! ' . $move->figure_from . ' -> ' . $action;
+            } else if ($move->collision == ChessEngine::MOVE_ILLEGAL) {
+                $message = 'Illegal move! ' . $move->figure_from . ' -> ' . $action;
+            } else if ($move->collision == ChessEngine::MOVE_ROADBLOCK) {
+                $message = 'Roadblock! ' . $move->figure_from . ' -> ' . $action;
+            }
+        } else {
+            $message = 'Wrong move: ' . $move->figure_from . ' -> ' . $action;
+        }
+    } else {
+        $message = 'Invalid move: ' . $action;
+    }
+    return $message;
+}
+
+$game = new ChessEngine;
+
+
+echo print_board($game->get_game_board());
+
+$stdin = fopen('php://stdin', 'r');
+stream_set_blocking($stdin, 0);
+
+$stty_config = shell_exec('stty -g');
+
+system('stty cbreak -echo');
+
+$action = '';
+echo "\e[0Gaction > " . $action;
+$exit = false;
+while(!$exit) {
+    $keypress = fgetc($stdin);
+    if ($keypress) {
+        $result = '';
+        if (ord($keypress) == 10) {
+            $result = perform_action($game, $action);
+            if ($result === 0) {
+                $exit = true;
+                $result = '';
+            } else {
+                echo "\e[0G\e[K";
+                echo "\e[26A";
+                echo print_board($game->get_game_board());
+                $action = '';
+                $keypress = '';
+            }
+        }
+
+        if (ord($keypress) == 127){
+            echo "\e[0G\e[K";
+            $action = substr($action, 0, -1);
+            $keypress = '';
+        }
+
+        echo "\e[0G\e[K";
+        $action .= $keypress;
+        echo "\e[0Gaction > " . $action . $result;
+    }
+}
+
+system('stty ' . $stty_config);
